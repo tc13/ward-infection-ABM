fa4c8c9b0e1387dfa8e4066c9b1d443c7e4b2aaa
Tom Crellen MORU <thomas.c@tropmedres.ac>
2019-01-16 15:26:56 +0700

    fourth commit, R0 calculator

--- klebs-ecoli-transmission.py
+++ klebs-ecoli-transmission.py
@@ -10,18 +10,20 @@ import sys
 import argparse
 
 #Argparse
-parser = argparse.ArgumentParser(description="Agent Based Model of AMR introduction and spread in a hospital ward. \n \n Author Tom Crellen (tomcrellen@gmail.com) MORU Postdoc")
+parser = argparse.ArgumentParser(description="Agent Based Models of AMR introduction and spread in a hospital ward. \n\nAuthor Tom Crellen (tomcrellen@gmail.com) MORU Postdoc. \n \n Models can 1) simulate cohort study or 2) calculate R0")
+#determine which model to run
+parser.add_argument('-m', '--mode', default=None, required=False, dest="mode", metavar="", help="[cohort / r0] Model section, cohort study or R0 simulator")
 #import patient length of stay distribution from the command line
-parser.add_argument('-l', '--lengthofstay', default=None, required=False, dest="los",metavar="",help="Path to file where each line is length of stay (days)")
-parser.add_argument('-i', '--iterations', default=350, required=False, dest="iter", metavar="", help="Number of model iterations (days)")
-parser.add_argument('-b', '--beds', default =8, required=False, dest="beds", metavar="", help="Number of beds in ward") 
-parser.add_argument('-r', '--entryrate', default =3, required=False, dest="entry", metavar="", help="Entry rate of patients per day (Poisson rate parameter")
-parser.add_argument('-k', '--hgtkleb', default=0.05, required=False, dest="HGT_kleb", metavar="", help="Probability of horizontal gene tranfer from K. pneumoniae to E. coli")
-parser.add_argument('-p','--masskleb', default=0.05, required=False, dest="PMA_kleb", metavar="", help="Probability of person-to-person transmission of K. pneumoniae")
-parser.add_argument('-e', '--hgtecoli', default= 0.0005, required=False, dest="HGT_ecoli", metavar="", help="Probability of horizontal gene transfer from E. coli to K. pneumoniae")
-parser.add_argument('-c', '--massecoli', default=0.01, required=False, dest="PMA_ecoli", metavar="", help="Probability of person-to-person transmission of E. coli")
-parser.add_argument('-x', '--importkleb', default=0.4, required=False, dest="import_kleb", metavar="", help="Probability that patient is colonized with K. pneumoniae on admission (imported case)")
-parser.add_argument('-y', '--importecoli', default=0.3, required=False, dest="import_ecoli", metavar="", help="Probability that patient is colonized with E. coli on admission (imported case)")
+parser.add_argument('-l', '--lengthofstay', default=None, required=False, dest="los",metavar="",help="Path to file where each line is length of stay in days (empirical distribution)")
+parser.add_argument('-i', '--iterations', default=365, required=False, dest="iter", metavar="", help="Number of model iterations / days (365)")
+parser.add_argument('-b', '--beds', default =8, required=False, dest="beds", metavar="", help="Number of beds in ward (8)") 
+parser.add_argument('-r', '--entryrate', default =3, required=False, dest="entry", metavar="", help="Entry rate of patients per day, Poisson rate parameter (3)")
+parser.add_argument('-k', '--hgtkleb', default=0.05, required=False, dest="HGT_kleb", metavar="", help="Probability of horizontal gene tranfer from K. pneumoniae to E. coli (0.05)")
+parser.add_argument('-p','--masskleb', default=0.05, required=False, dest="PMA_kleb", metavar="", help="Probability of person-to-person transmission of K. pneumoniae (0.05)")
+parser.add_argument('-e', '--hgtecoli', default= 0.0005, required=False, dest="HGT_ecoli", metavar="", help="Probability of horizontal gene transfer from E. coli to K. pneumoniae (0.0005)")
+parser.add_argument('-c', '--massecoli', default=0.01, required=False, dest="PMA_ecoli", metavar="", help="Probability of person-to-person transmission of E. coli (0.01)")
+parser.add_argument('-x', '--importkleb', default=0.4, required=False, dest="import_kleb", metavar="", help="Probability that patient is colonized with K. pneumoniae on admission (imported case) (0.4)")
+parser.add_argument('-y', '--importecoli', default=0.3, required=False, dest="import_ecoli", metavar="", help="Probability that patient is colonized with E. coli on admission (imported case) (0.3)")
 args = parser.parse_args()
 
 #Collect arguments passed from the command line
@@ -33,6 +35,8 @@ HGT_klebs = float(args.HGT_kleb)
 PMA_klebs = float(args.PMA_kleb)
 HGT_ecoli = float(args.HGT_ecoli)
 PMA_ecoli = float(args.PMA_ecoli)
+import_klebs = float(args.import_kleb)
+import_ecoli = float(args.import_ecoli)
 
 #Process input lengths of stay
 los_dist = []
@@ -46,13 +50,13 @@ else:
         los_dist = [3, 4, 3, 5, 12, 29, 12, 4, 6, 5, 22, 4, 5, 16, 11, 9, 4, 5, 5, 5, 6, 4, 10, 66, 4, 6, 4, 8, 4, 12, 14, 3, 5, 5, 8, 10, 9, 8, 16, 38, 3, 5, 47, 15, 9, 3, 3, 5, 7, 7, 9, 4, 7, 4, 5, 3, 2, 3, 3, 9, 11, 28, 21, 7, 4, 17, 8, 5, 6, 5, 4, 4, 1, 6, 20, 13, 11, 7, 8, 19, 5, 22, 8, 18, 6, 9, 5, 4, 6, 6, 19, 17, 5, 3, 11, 26, 3, 12, 7, 7, 11, 8, 21, 6, 8, 4, 4, 31, 11, 3, 6, 14, 10, 3, 11, 6, 12, 5, 14, 6, 5, 5, 7, 3, 6, 3, 3, 6, 8, 2, 4, 10, 6, 11, 51, 11, 2, 11, 3, 15, 4, 56, 8, 3, 4, 27, 3, 8, 18, 3, 10, 7, 19, 6, 3, 3, 5, 16, 8, 4, 16, 5, 58, 3, 3, 2, 34, 13, 4, 3, 8, 2, 5, 9, 10, 3, 4, 4, 19, 6, 8, 8, 7, 8, 10, 3, 8, 1, 14, 2, 5, 8, 7, 3, 7, 9, 5, 3, 3, 3, 2, 2, 43, 8, 4, 40, 7, 4, 3, 60, 7, 9, 3, 3, 10, 6, 2, 9, 4, 8, 4, 4, 2, 2, 3, 4, 5, 5, 5, 32, 11, 3, 8, 4, 3, 2, 3, 5, 9, 3, 6, 4, 5, 25, 7, 6, 5, 20, 4, 5, 3, 54, 6, 32, 20, 6, 4, 6, 3, 7, 3, 6, 4, 4, 20, 17, 16, 3, 12, 27, 31, 5, 48, 5, 3, 3, 10, 6, 6, 5, 4, 8, 37, 8, 3, 8, 7, 4, 4, 3, 10, 20, 3, 3, 10, 4, 5, 20, 3, 29, 5, 3, 2, 15, 7, 25, 3, 30, 42, 21, 57, 41, 3, 3, 5, 13, 5, 5, 20, 5, 34, 4, 4, 4, 6, 8, 27, 14, 5, 5, 54, 34, 22]
 
 class ward:
-        def __init__(self, n_iterations=n_iterations, entry_rate=entry_rate, beds=beds, los_dist=los_dist, HGT_klebs=HGT_klebs, PMA_klebs=PMA_klebs, HGT_ecoli=HGT_ecoli, PMA_ecoli=PMA_ecoli):
+        def __init__(self, n_iterations=n_iterations, entry_rate=entry_rate, beds=beds, los_dist=los_dist, HGT_klebs=HGT_klebs, PMA_klebs=PMA_klebs, HGT_ecoli=HGT_ecoli, PMA_ecoli=PMA_ecoli, import_klebs=import_klebs, import_ecoli=import_ecoli):
                 self.n_iterations = n_iterations
                 self.los = numpy.array(los_dist)
                 self.patients = {}
                 self.entry_rate = entry_rate
-                self.entry_risk_klebs = 0.4
-                self.entry_risk_ecoli = 0.3
+                self.entry_risk_klebs = import_klebs
+                self.entry_risk_ecoli = import_ecoli
                 #number of new patients admitted each day, average is rate parameter of poisson
                 self.new_patients = numpy.random.poisson(entry_rate, n_iterations)
                 self.empty_beds = beds
@@ -173,9 +177,84 @@ class ward:
                                 #print output to command line
                                 print(str(day) + "\t" + str(len(klebs_uncolonised)) + "\t" + str(len(klebs_colonised)) + "\t" + str(len(ecoli_uncolonised)) + "\t" + str(len(ecoli_colonised))) 
 
+#################
+# R0 calculator #
+#################
 
-#Run model
-run=ward()
-#print column headers
-print("day" + "\t" + "klebsiella_uncolonised" + "\t" + "klebsiella_colonised" + "\t" + "ecoli_uncolonised" + "\t" + "ecoli_colonised")
-run.admit()
+class R0:
+        def __init__(self, beds=beds, los_dist=los_dist, p_acquire=PMA_klebs):
+                self.los = numpy.array(los_dist)
+                self.patients = {}
+                self.beds = beds
+                self.p_acquire = p_acquire
+                self.R = 0
+                self.patient_zero = numpy.random.choice(self.los)
+                self.occupied_beds = []
+
+        def admit(self):
+                for n in range(1, self.beds):
+                        #give characteristics to new patients
+                        name = "1"+"."+str(n)
+                        los = numpy.random.choice(self.los)
+                        discharge_day = 1+los
+                        #patient dict. ID is key, values are day of entry, discharge, klebs dict, ecoli dict
+                        self.patients[name] = [0, discharge_day, 0]
+                
+                #give characterics to colonised patient
+                self.patients["zero"] = [0, self.patient_zero+1, 1]
+
+                #list of occupied beds
+                self.occupied_beds = self.patients.keys()
+
+        def spread(self):
+                for day in range(1, self.patient_zero+1):
+                        
+                        #remove patients where discharge day == current day
+                        remove = [bed for bed, date in self.patients.items() if date[1]==day]
+                        self.occupied_beds = [bed for bed in self.occupied_beds if bed not in remove]
+                        
+                        #admit new patients when old ones have been discharged
+                        for n in range(len(remove)):
+                                name = str(day)+"."+str(n)
+                                los = numpy.random.choice(self.los)
+                                discharge_day = day+los
+                                self.patients[name] = [day, discharge_day, 0]
+
+                        #infection process
+                        susceptible = []
+                        
+                        #check which patients are susceptible and infected
+                        for agent in self.occupied_beds:
+
+                                if self.patients[agent][2] == 0:   
+                                        susceptible.append(agent)
+
+                        #infection is a Bernoulli trial
+                        outcome = numpy.random.binomial(n=1, p=self.p_acquire, size=len(susceptible))
+                        #indexes of successful transmission events (1)                     
+                        outcome_index = [o for o,x in enumerate(outcome) if x==1]
+                        #update patient dictionary
+                        if outcome_index:
+                                for j in outcome_index:
+                                        self.patients[susceptible[j]][2] = 1
+                        
+                        #add to R the number of new colonizations
+                        self.R += sum(outcome)
+
+                print str(self.patient_zero) + "\t" +  str(self.R)
+#Run model based on mode argument
+#Cohort simulation
+if str.lower(args.mode)=="cohort":
+        run=ward()
+        #print column headers
+        print("day" + "\t" + "klebsiella_uncolonised" + "\t" + "klebsiella_colonised" + "\t" + "ecoli_uncolonised" + "\t" + "ecoli_colonised")
+        run.admit()
+
+#R0 simulation
+elif str.lower(args.mode)=="r0":
+        run=R0()
+        run.admit()
+        run.spread()
+
+else:
+        print "not a valid model selection. Use -m [cohort / R0]"
